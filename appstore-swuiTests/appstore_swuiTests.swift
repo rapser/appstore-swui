15af5e3f7c24015773a2bfbb9c7e0034211b2133
Miguel Angel Tomairo <matomairo@gmail.com>
2020-05-07 00:11:13 -0500

    Initial Commit

---
+++ appstore-swuiTests/appstore_swuiTests.swift
@@ -0,0 +1,34 @@
+//
+//  appstore_swuiTests.swift
+//  appstore-swuiTests
+//
+//  Created by miguel tomairo on 5/7/20.
+//  Copyright © 2020 Rapser. All rights reserved.
+//
+
+import XCTest
+@testable import appstore_swui
+
+class appstore_swuiTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
